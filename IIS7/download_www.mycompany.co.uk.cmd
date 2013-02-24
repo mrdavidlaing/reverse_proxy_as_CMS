cf708b3f487a0659af1f23cb52b8f9341818c419
David Laing <david@davidlaing.com>
2013-02-24 18:43:34 +0000

    Use www.reverse-proxy-as-cms.info as IIS7 sample

--- IIS7/download_www.mycompany.co.uk.cmd
+++
@@ -1,1 +0,0 @@
-"c:\Program Files (x86)\GnuWin32\bin\wget.exe" -mk http://www.mycompany.co.uk --directory-prefix=archive.mycompany.co.uk --html-extension
---
+++ IIS7/download_www.reverse-proxy-as-cms.info.cmd
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\GnuWin32\bin\wget.exe" -mk http://www.reverse-proxy-as-cms.info --page-requisites --directory-prefix=archive.www.reverse-proxy-as-cms.info --html-extension
