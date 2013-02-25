f46ba71befa251ceac0a4275d7d670747267ee3f
David Laing <david@davidlaing.com>
2013-02-25 01:47:44 +0000

    Inject navbar into FAQ

---
+++ IIS7/InjectHTMLUrlRewriteProvider/InjectHTMLUrlRewriteProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Web.Iis.Rewrite;
+
+namespace InjectHTMLUrlRewriteProvider
+{
+    public class InjectHTMLUrlRewriteProvider: IRewriteProvider, IProviderDescriptor
+    {
+        private string _replacePattern;
+        public void Initialize(IDictionary<string, string> settings, IRewriteContext rewriteContext)
+        {
+            if (!settings.TryGetValue("replacePattern", out _replacePattern) || string.IsNullOrEmpty(_replacePattern))
+                throw new ArgumentException("replacePattern provider setting is required and cannot be empty");
+        }
+
+        
+        public string Rewrite(string url)
+        {
+            var webClient = new WebClient();
+            return webClient.DownloadString(url);
+        }
+
+        public IEnumerable<SettingDescriptor> GetSettings()
+        {
+            yield return new SettingDescriptor("replacePattern", "pattern to replace");
+        }
+    }
+}
